56775eeab2e5fcc61ba2e3573d13ac115df9aa26
sjoerd <sjoerdisverige@gmail.com>
2024-12-01 13:58:28 +0100

    Add project code and start of Day 1

---
+++ Days/Day01/Day1_part1.cs
@@ -0,0 +1,12 @@
+namespace AdventOfCode2024
+{
+    public static class Day01
+    {
+        public static void Solve()
+        {
+            // string[] lines = File.ReadAllLines("Days/Day01/input_part1.txt");
+            string[] lines = File.ReadAllLines("Days/Day01/example.txt");
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AdventOfCode2024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day01.Solve();
+            //Day02.Solve();
+            //Day03.Solve();
+            // Automatically runs each day’s solution
+        }
+
+    }
+}
