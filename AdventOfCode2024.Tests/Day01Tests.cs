4eaa9d48651c104dc4699a54333110c723c0f520
sjoerd <sjoerdisverige@gmail.com>
2024-12-02 20:01:07 +0100

    Day01 part 1

--- AdventOfCode2024.Tests/Day01Tests.cs
+++ AdventOfCode2024.Tests/Day01Tests.cs
@@ -7,9 +7,20 @@ namespace AdventOfCode2024.Tests;
 public class Day01Tests
 {
     [Fact]
+    public void TestExample1()
+    {
+        var filePath = @"C:\GitHub\AdventOfCode2024\InputDocuments\day1_test_data.txt";
+        var day01 = new Day01();
+        var sum = day01.Part1(filePath);
+        System.Diagnostics.Debug.WriteLine($"The sum is {sum}");
+    }
+
+    [Fact]
     public void TestPart1()
     {
-        System.Diagnostics.Debug.WriteLine("Sjoerd was here!");
-        var result = Day01.Part1();
+        var filePath = @"C:\GitHub\AdventOfCode2024\InputDocuments\day1_part1.txt";
+        var day01 = new Day01();
+        var sum = day01.Part1(filePath);
+        System.Diagnostics.Debug.WriteLine($"The sum is {sum}");
     }
 }
--- AdventOfCode2024/Day01.cs
+++ AdventOfCode2024/Day01.cs
@@ -1,14 +1,49 @@
-
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2024
 {
     public class Day01
     {
-        public static bool Part1()
+        public Regex regex = new(@"(\d+)\s+(\d+)");
+        public int Part1(string textFilePath)
         {
-            System.Diagnostics.Debug.WriteLine("Henk was here!");
-            string[] lines = File.ReadAllLines(@"C:\GitHub\AdventOfCode2024\InputDocuments\day1_test_data.txt");
-            return true;
+            string[] lines = File.ReadAllLines(textFilePath);
+
+            var firstList = new List<int>();
+            var secondList = new List<int>();
+            foreach (var line in lines)
+            {
+                var match = regex.Match(line);
+
+                if (!match.Success)
+                {
+                    System.Diagnostics.Debug.WriteLine($"Match failed for {line}");
+                    continue;
+                }
+
+                bool firstSuccess = int.TryParse(match.Groups[1].Value, out int firstNumber);
+                bool secondSuccess = int.TryParse(match.Groups[2].Value, out int secondNumber);
+                if (!firstSuccess || !secondSuccess)
+                {
+                    System.Diagnostics.Debug.WriteLine($"Parsing failed for one of the numbers in {line}");
+                    continue;
+                }
+
+                firstList.Add(firstNumber);
+                secondList.Add(secondNumber);
+            }
+
+            firstList.Sort();
+            secondList.Sort();
+
+            var sum = 0;
+            for (int i = 0; i < firstList.Count; i++)
+            {
+                var difference = Math.Abs(firstList[i] - secondList[i]);
+                sum += difference;
+            }
+
+            return sum;
         }
     }
 }
