4b9a06ea1d43d317d7ca9d3d07ae3de7b5dce1f5
sjoerd <sjoerdisverige@gmail.com>
2024-12-02 20:31:54 +0100

    day1 part 2

--- AdventOfCode2024.Tests/Day01Tests.cs
+++ AdventOfCode2024.Tests/Day01Tests.cs
@@ -18,9 +18,27 @@ public class Day01Tests
     [Fact]
     public void TestPart1()
     {
-        var filePath = @"C:\GitHub\AdventOfCode2024\InputDocuments\day1_part1.txt";
+        var filePath = @"C:\GitHub\AdventOfCode2024\InputDocuments\day1_input.txt";
         var day01 = new Day01();
         var sum = day01.Part1(filePath);
+        System.Diagnostics.Debug.WriteLine($"The sum is {sum}");
+    }
+
+    [Fact]
+    public void TestExample2()
+    {
+        var filePath = @"C:\GitHub\AdventOfCode2024\InputDocuments\day1_test_data.txt";
+        var day01 = new Day01();
+        var sum = day01.Part2(filePath);
+        System.Diagnostics.Debug.WriteLine($"The sum is {sum}");
+    }
+
+    [Fact]
+    public void TestPart2()
+    {
+        var filePath = @"C:\GitHub\AdventOfCode2024\InputDocuments\day1_input.txt";
+        var day01 = new Day01();
+        var sum = day01.Part2(filePath);
         System.Diagnostics.Debug.WriteLine($"The sum is {sum}");
     }
 }
--- AdventOfCode2024/Day01.cs
+++ AdventOfCode2024/Day01.cs
@@ -7,6 +7,49 @@ namespace AdventOfCode2024
         public Regex regex = new(@"(\d+)\s+(\d+)");
         public int Part1(string textFilePath)
         {
+            (List<int> firstList, List<int> secondList) = GetLists(textFilePath);
+
+            var sum = 0;
+            for (int i = 0; i < firstList.Count; i++)
+            {
+                var difference = Math.Abs(firstList[i] - secondList[i]);
+                sum += difference;
+            }
+
+            return sum;
+        }
+
+        public int Part2(string textFilePath)
+        {
+            (List<int> firstList, List<int> secondList) = GetLists(textFilePath);
+
+            var amountDict = new Dictionary<int, int>();
+            // Create a dictionary from secondList to count the amounts
+            foreach (int item in secondList)
+            {
+                if (!amountDict.ContainsKey(item))
+                    amountDict[item] = 0;
+                amountDict[item] += 1;
+            }
+
+            // Loop through firstList to get the sum
+            var sum = 0;
+            foreach (int item in firstList)
+            {
+                int amount;
+                if (amountDict.ContainsKey(item))
+                    amount = amountDict[item];
+                else
+                    amount = 0;
+
+                sum += item * amount;
+            }
+
+            return sum;
+        }
+
+        private (List<int>, List<int>) GetLists(string textFilePath)
+        {
             string[] lines = File.ReadAllLines(textFilePath);
 
             var firstList = new List<int>();
@@ -36,14 +79,8 @@ namespace AdventOfCode2024
             firstList.Sort();
             secondList.Sort();
 
-            var sum = 0;
-            for (int i = 0; i < firstList.Count; i++)
-            {
-                var difference = Math.Abs(firstList[i] - secondList[i]);
-                sum += difference;
-            }
-
-            return sum;
+            return (firstList, secondList);
         }
+
     }
 }
